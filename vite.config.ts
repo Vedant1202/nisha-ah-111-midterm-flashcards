fac008371fa29b114f954e7b6a2337c330918187
Vedant  Nandoskar <vedant.nandoskar@gmail.com>
2026-03-11 16:03:31 -0500

    update readme

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/nisha-ah-111-midterm-flashcards/",
   plugins: [react(), tailwindcss()],
 });
